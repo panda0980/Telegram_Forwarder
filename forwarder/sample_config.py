5fea6f06a36ae9fac8e62d690f4a93c996901b74
panda0980 <103883208+panda0980@users.noreply.github.com>
2022-12-20 23:41:51 +0530

    Update sample_config.py

--- forwarder/sample_config.py
+++ forwarder/sample_config.py
@@ -15,12 +15,12 @@ class Config(object):
     REMOVE_TAG = False
 
     # REQUIRED
-    API_KEY = "617722711:AAHysCHsVcQja6LOlHa88101lKjrxez80-M"  # API key obtained from BotFather
-    OWNER_ID = "582884567"  # If you dont know, run the bot and do /id in your private chat with the bot
+    API_KEY = "5813212548:AAG1503Ilf2se-uiHkWwNSxPSr20v0yL6Oo"  # API key obtained from BotFather
+    OWNER_ID = "1983530070"  # If you dont know, run the bot and do /id in your private chat with the bot
 
     # FOR AUTOMATICALLY FORWARDING MESSAGES
-    FROM_CHATS = [-1001234704297]  # List of chat id's to forward messages from
-    TO_CHATS = [-1001128355490]  # List of chat id's to forward messages to
+    FROM_CHATS = [-1001512321422]  # List of chat id's to forward messages from
+    TO_CHATS = [-100691135626]  # List of chat id's to forward messages to
 
     # FOR WEBHOOKS
     WEBHOOK = False
